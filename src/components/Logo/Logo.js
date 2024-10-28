ee7f2ad0db274c8d46c44d9b7bdd2f40bc4adf35
erik <erik.cr42@gmail.com>
2024-10-28 20:15:34 -0300

    Creates Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-portfolio-logo-design-hd-png-download.png" className="br-100 ba h3 w3 dib" alt="logo" />
+        </div>
+    );
+}
+
+export default Logo;
