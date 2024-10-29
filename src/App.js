ffc29d370656e0e0cbdf32d7bda7cd17033515b4
erik <erik.cr42@gmail.com>
2024-10-29 18:45:16 -0300

    Setting up ImageLinkForm and Rank

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
 
 class App extends Component {
   render() {
@@ -9,8 +12,9 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
-                {/*<ImageLinkForm/>
-                <FaceRecognition/> */}
+        <ImageLinkForm/>
+        <Rank />
+                { /*<FaceRecognition/> */}
       </div>
     );
   }
